8e24eb11fba566ae45c58752bb6d52f76128eee1
Sam Leonard <samuel.leonard@student.manchester.ac.uk>
2021-04-09 15:51:36 +0100

    Added code for video.

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,69 @@
 use rgb::ComponentBytes;
 use std::{
-    fs::File,
-    io::{BufWriter, Write},
-    path::Path,
+    fs::{self,File},
+    io::BufWriter,
+    path::PathBuf,
 };
 
-use anyhow::Result;
+use anyhow::{Result,Context};
 use rayon::prelude::*;
 
 type Pixel = rgb::RGB<u8>;
-type ComponentFunc = dyn Fn(u32, u32) -> bool;
-type Row = Box<[Pixel]>;
 
-const WIDTH: u32 = 256;
+const WIDTH: i32 = 256;
 
 fn main() -> Result<()> {
-    // setup the file for buffered writing
-    let path = Path::new("render.png");
-    let file = File::create(path)?;
-    let w = &mut BufWriter::new(file);
+    let frame_path = PathBuf::from("./frames/");
+    fs::create_dir(&frame_path).context("failed to crate frames directory.")?;
 
-    // setup the PNG encoder / stram
-    let mut encoder = png::Encoder::new(w, WIDTH, WIDTH);
-    encoder.set_color(png::ColorType::RGB);
-    encoder.set_depth(png::BitDepth::Eight);
-    let mut writer = encoder.write_header()?;
-    let mut stream = writer.stream_writer();
+    for (i, z) in (0..=WIDTH).enumerate() { //((-WIDTH / 2)..=(WIDTH / 2)).enumerate() {
+        // setup the file for buffered writing
+        let mut frame = frame_path.clone();
+        frame.push(format!("{:05}.png", i));
+        let file = File::create(frame)?;
+        let w = &mut BufWriter::new(file);
 
-    // generate the pixels
-    let fr = |x, y| (x * y) & 64 != 0;
-    let fg = |x, y| (x | y) % 17 == 0;
-    let fb = |x, y| (x ^ y) & 32 == 0;
-    let rows: Vec<_> = (0..WIDTH).into_par_iter().map(|y| gen_row(y, (&fr, &fg, &fb))).collect();
+        // setup the PNG encoder / stram
+        let mut encoder = png::Encoder::new(w, WIDTH as u32, WIDTH as u32);
+        encoder.set_color(png::ColorType::RGB);
+        encoder.set_depth(png::BitDepth::Eight);
+        let mut writer = encoder.write_header()?;
 
-    // write the rows out to disk
-    for row in rows {
-        stream.write(row.as_bytes())?;
+        // generate the pixels
+        /*
+        let fr = |x, y| if (x * y) & 64 != 0 { 0xFF } else { 0x00 };
+        let fg = |x, y| if (x | y) % 17 == 0 { 0xFF } else { 0x00 };
+        let fb = |x, y| if (x ^ y) & 32 == 0 { 0xFF } else { 0x00 };
+        */
+
+        /*
+        let ff = |x, y, z| {
+            let modulus = (x - 128);
+            let left = (y - 128) * z;
+            if modulus != 0 && (left % modulus) == 0 { 0xFF } else { 0x00 }
+        };
+        */
+
+        let ff = |x: i32, y: i32, z: i32| if (x * y) & ((x ^ y) < z) as u8 as i32 == 0 { 0xFF } else { 0x00 };
+
+        let pixels: Vec<_> = (0..WIDTH*WIDTH)
+            .into_par_iter()
+            .map(|i| {
+                let x = i / WIDTH;
+                let y = i % WIDTH;
+
+                let v = ff(x, y, z);
+                Pixel {
+                    r: v, //fr(x, y),
+                    g: v, //fg(x, y),
+                    b: v, //fb(x, y),
+                }
+            })
+        .collect();
+
+        // write the rows out to disk
+        writer.write_image_data(pixels.as_bytes())?;
     }
 
-    // finalise the PNG
-    stream.finish()?;
-
     Ok(())
-}
-
-fn gen_row(y: u32, funcs: (&ComponentFunc, &ComponentFunc, &ComponentFunc)) -> Row {
-    let (fr, fg, fb) = funcs;
-
-    (0..WIDTH)
-        .map(|x| {
-            Pixel {
-                r: if fr(x, y) { 0xFF } else { 0x00 },
-                g: if fg(x, y) { 0xFF } else { 0x00 },
-                b: if fb(x, y) { 0xFF } else { 0x00 },
-            }
-        })
-        .collect::<Vec<_>>()
-        .into_boxed_slice()
 }
